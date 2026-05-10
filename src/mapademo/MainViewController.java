189e4c965baaeb2d34483f91eefe981710424f3a
vgarcol <vgarcol@upv.edu.es>
2026-05-10 17:56:56 +0200

    Crea MainShell con BorderPane y método cargarVista

--- src/mapademo/MainViewController.java
+++ src/mapademo/MainViewController.java
@@ -1,26 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
 package mapademo;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 
-/**
- * FXML Controller class
- *
- * @author david
- */
 public class MainViewController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private BorderPane rootPane;  // El BorderPane principal del FXML
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        // Se ejecuta automáticamente al cargar el FXML
+        // De momento vacío, aquí luego cargaremos la vista de Login
+    }
+
+    // Carga cualquier vista FXML en el centro del BorderPane
+    public void cargarVista(String fxmlPath) {
+        try {
+            Pane vista = FXMLLoader.load(getClass().getResource(fxmlPath));
+            rootPane.setCenter(vista);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
